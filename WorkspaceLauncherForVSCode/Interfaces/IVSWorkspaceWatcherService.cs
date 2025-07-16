efa3df62d49f4fdabbc5534dcf0cc2427ca19e35
tanchekwei <weiwei1@live.com.my>
2025-07-16 22:30:21 +0800

    refactor: Implement background refresh and watcher service

---
+++ WorkspaceLauncherForVSCode/Interfaces/IVSWorkspaceWatcherService.cs
@@ -0,0 +1,13 @@
+// Modifications copyright (c) 2025 tanchekwei
+// Licensed under the MIT License. See the LICENSE file in the project root for details.
+using System;
+
+namespace WorkspaceLauncherForVSCode.Interfaces
+{
+    public interface IVSWorkspaceWatcherService
+    {
+        event EventHandler TriggerRefresh;
+        void StartWatching();
+        void StopWatching();
+    }
+}
